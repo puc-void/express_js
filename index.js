266d30ab56bcb1b84bd214f374d443d29510da3b
puc-void <215346186+puc-void@users.noreply.github.com>
2026-05-14 09:11:22 +0600

    My First Express Application

---
+++ index.js
@@ -0,0 +1,14 @@
+var express = require('express')
+app = express();
+
+
+
+app.get("/", function (req, res) {
+    res.send("Hello World!");
+});
+
+
+
+app.listen(8080, function () {
+    console.log("serveer is running");
+});
